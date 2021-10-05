83a11f9a9d8fedf5024d9e40f7f97702abc4d48b
ih810 <ivanhong810@gmail.com>
2021-10-05 14:02:10 +0800

    quick sort??????

--- Quick_Sort/quickSort.js
+++ Quick_Sort/quickSort.js
@@ -1,14 +1,17 @@
-
+var count = 0
 //action
 function swap(items, leftIndex, rightIndex){
+    console.log('items in swap1', items)
     var temp = items[leftIndex];
     items[leftIndex] = items[rightIndex];
     items[rightIndex] = temp;
+    console.log('items in swap2', items)
+    count++
 }
 
 //break half
 function partition(items, left, right) {
-    var mid   = items[Math.floor((right + left) / 2)], //middle element
+    var mid     = items[Math.floor((right + left) / 2)], //middle element
         i       = left, //left pointer
         j       = right; //right pointer
     while (i <= j) {
@@ -19,6 +22,7 @@ function partition(items, left, right) {
             j--;
         }
         if (i <= j) {
+            console.log('item in partition', items)
             swap(items, i, j); //sawpping two elements
             i++;
             j--;
@@ -39,10 +43,10 @@ function quickSort(items, left, right) {
             quickSort(items, index, right);
         }
     }
-    console.log('items',items)
     return items;
 }
 // first call to quick sort
-var items = [ 82, 64, 35, 79, 106, 7695, 19];
+var items = [9,8,7,6,5,4,3,2,1];
 var sortedArray = quickSort(items, 0, items.length - 1);
+console.log('count', count)
 // console.log(sortedArray); //prints [2,3,5,6,7,9]
